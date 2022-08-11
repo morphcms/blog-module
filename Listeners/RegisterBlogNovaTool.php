f89bee04e5a5a72ce02616ac4cfc4466d1aa12d9
Codrin Axinte <codrin.axinte@outlook.com>
2022-08-11 15:03:22 +0300

    Update RegisterBlogNovaTool.php

--- Listeners/RegisterBlogNovaTool.php
+++ Listeners/RegisterBlogNovaTool.php
@@ -6,7 +6,7 @@ use Modules\Blog\Nova\NovaBlogTool;
 
 class RegisterBlogNovaTool
 {
-    public function __invoke()
+    public function __invoke(): array
     {
         return [
             NovaBlogTool::make(),
